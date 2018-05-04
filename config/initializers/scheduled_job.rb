f9c5cb294620c5b2cfb6fab39867f5670ab41826
Neeraj Singh <neeraj@bigbinary.com>
2018-05-04 10:53:01 -0700

    comment out purge record for now

--- config/initializers/scheduled_job.rb
+++ config/initializers/scheduled_job.rb
@@ -1,6 +1,6 @@
 # Schedule jobs when Rails starts. Added code to not run jobs for rake and delayed job worker tasks.
 if Delayed::Worker.delay_jobs && !($PROGRAM_NAME =~ /(rake|delayed_job)(.rb)?$/)
-  PurgeRecordJob.schedule_job
+  #PurgeRecordJob.schedule_job
 end
 
 ScheduledJob.configure do |config|
