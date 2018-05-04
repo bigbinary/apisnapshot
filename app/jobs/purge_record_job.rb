95dd913ab78d38bb6c05f71542a2a863ce0f4b0b
Neeraj Singh <neeraj@bigbinary.com>
2018-05-03 22:54:40 -0700

    indentation

--- app/jobs/purge_record_job.rb
+++ app/jobs/purge_record_job.rb
@@ -5,12 +5,12 @@ class PurgeRecordJob
     include ::ScheduledJob
 
     def perform
-        ScheduledJob.logger.info "Going to purge old records"
-        ApiResponse.where("created_at < ? ", RETAIN_RECORDS_FOR_IN_DAYS.days.ago).delete_all
+      ScheduledJob.logger.info "Going to purge old records"
+      ApiResponse.where("created_at < ? ", RETAIN_RECORDS_FOR_IN_DAYS.days.ago).delete_all
     end
 
     # run it every one hour
     def self.time_to_recur(last_run_at)
-        last_run_at + 1.hour
+      last_run_at + 1.hour
     end
 end
--- config/initializers/scheduled_job.rb
+++ config/initializers/scheduled_job.rb
@@ -1,9 +1,9 @@
 # Schedule jobs when Rails starts. Added code to not run jobs for rake and delayed job worker tasks.
 if Delayed::Worker.delay_jobs && !($PROGRAM_NAME =~ /(rake|delayed_job)(.rb)?$/)
-    PurgeRecordJob.schedule_job
+  PurgeRecordJob.schedule_job
 end
 
 
 ScheduledJob.configure do |config|
-    config.logger = Rails.logger
+  config.logger = Rails.logger
 end
