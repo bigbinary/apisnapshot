946179c33c2b01fc95b886c658975abab85d9688
Vipul A M <vipulnsward@gmail.com>
2018-05-14 18:40:18 +0530

    Fix the delayed_job table may not exists and db:setup script fails without the check

--- config/initializers/scheduled_job.rb
+++ config/initializers/scheduled_job.rb
@@ -1,5 +1,5 @@
 # Schedule jobs when Rails starts. Added code to not run jobs for rake and delayed job worker tasks.
-if Delayed::Worker.delay_jobs && !($PROGRAM_NAME =~ /(rake|delayed_job)(.rb)?$/)
+if Delayed::Worker.delay_jobs && !($PROGRAM_NAME =~ /(rake|delayed_job)(.rb)?$/) && ApplicationRecord.connection.table_exists?("delayed_jobs")
   PurgeRecordJob.schedule_job
 end
 
